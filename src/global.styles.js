1688581d0b0d5b14f79a3e5c710a2fced8509f13
Diablobait <thor.stg@gmail.com>
2020-05-14 17:04:59 +0200

    📁 Structure done

---
+++ src/global.styles.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  *,
+  *::after,
+  *::before {
+    margin: 0px;
+    padding: 0px;
+    box-sizing: inherit;
+  }
+  html {
+    font-size: 62.5%;
+  }
+  body {
+    box-sizing: border-box;
+    font-family: sans-serif;
+    background: #F1EFE1;
+  }
+`;
+
+export default GlobalStyles;
---
+++ src/utils/api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const baseUrl = 'http://raw.githubusercontent.com/rrafols/mobile_test/master/data.json';
+
+const axiosInstance = axios.create({
+  baseURL: baseUrl
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default () => {
+  return axiosInstance.get('');
+}
